1ef1130dc2d54e77fde79b755b19600e4a6418bf
Gabriel Arcanjo <garcanjo938@gmail.com>
2022-08-21 10:17:58 -0300

    Soma em C

--- quest1-somafacil.c
+++ quest1-somafacil.c
@@ -1,9 +1,10 @@
-#include <stdio.h>
+/*id do problema: 134*/
 
+#include <stdio.h>
 int main(){
 
-int A, B;
-scanf("%d%d", &A, &B);
-printf("%d", A+B);
+    int A, B;
+    scanf("%d%d", &A, &B);
+    printf("%d", A+B);
 
 }
