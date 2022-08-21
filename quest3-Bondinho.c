e3fa8c3ce044581ca82081e39858d9e11fc7b4d2
Gabriel Arcanjo <garcanjo938@gmail.com>
2022-08-20 22:44:21 -0300

    Bondinho em C

---
+++ quest3-Bondinho.c
@@ -0,0 +1,13 @@
+/* id do problema: 13*/
+#include <stdio.h>
+int main(){
+    int A, M;
+    int X;
+    scanf("%d%d", &A, &M);
+    X = A + M;
+    if( X > 50){
+        printf("N");
+    }else{
+        printf("S");
+    };
+}
