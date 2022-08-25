f12c701cd1d375ec3816a39f1ffc3c28c0cde5a9
Gabriel Arcanjo <garcanjo938@gmail.com>
2022-08-25 18:46:32 -0300

    Repetir X vezes em C

---
+++ Repetir_X_vezes/Repetir_x_vezes.c
@@ -0,0 +1,9 @@
+/* problema do curso de introdução à programação do Neps Academy*/
+#include <stdio.h>
+int main(){
+    int X, i;
+    scanf("%d", &x);
+    for(i=1; i<N; i++){
+        printf("NepsAcademy eh Sucesso\n");
+    }
+}
