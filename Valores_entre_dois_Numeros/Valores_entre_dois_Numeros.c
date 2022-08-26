76bd6eda5b35aea6eb2c510344b7647438761c56
Gabriel Arcanjo <garcanjo938@gmail.com>
2022-08-26 15:50:06 -0300

    Valores entre Dois Números em C

--- Valores_entre_dois_Numeros/Valores_entre_dois_Numeros.c
+++ Valores_entre_dois_Numeros/Valores_entre_dois_Numeros.c
@@ -1,9 +1,18 @@
 /* problema do curso de introdução à informática do Neps Academy */
-#include <stdio.h>
-int main(){
-    int A, B, i;
-    scanf("%d%d", &A, &B);
-    for (i=A; i<B; i++){
-        printf("%d"i);
-    }
+#include <stdio.h> 
+ 
+int main(){ 
+  int A, B, i; 
+  scanf("%d %d", &A, &B); 
+  if(A < B){ 
+    i = A; 
+    for (i=A; i<=B; i++){  
+      printf("%d ",i); 
+    } 
+  }else{  
+    for (i=B; i<=A; i++){  
+      printf("%d ",i);  
+    }  
+  }  
 }
+
