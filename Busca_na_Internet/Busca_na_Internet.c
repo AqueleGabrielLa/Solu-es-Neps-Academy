192fb27be3e3a2e7653be8c5ca3f252d0180310d
Gabriel Arcanjo <garcanjo938@gmail.com>
2022-09-04 14:51:01 -0300

    Busca na Internet em C

---
+++ Busca_na_Internet/Busca_na_Internet.c
@@ -0,0 +1,11 @@
+/* id do problema: 515 */
+#include <stdio.h>
+int main(){
+    int t, slt, plt;
+    scanf("%d", &t);
+    slt = t * 2;
+    plt = slt + slt;
+    printf("%d", plt);
+
+    return 0;
+}
