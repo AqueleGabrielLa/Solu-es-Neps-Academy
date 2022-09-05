23e3478f73c7d2a879d49df52947f5ff4dd4ea73
Gabriel Arcanjo <garcanjo938@gmail.com>
2022-09-05 15:19:24 -0300

    Inverso em C

---
+++ Inverso/Inverso.c
@@ -0,0 +1,16 @@
+/* problema do curso de introdução à programação do Neps Academy*/
+#include <stdio.h>
+int main(){
+    int vetor[10];
+    int i;
+    for(i=0; i<10; i++){
+        scanf("%d", &vetor[i]);
+    }
+    if(vetor[i] = vetor[10]){
+        for(i = 10; i >= 1; i--){
+            printf("%d\n", vetor[i - 1]);   
+        }
+    }
+
+    return 0;
+}
