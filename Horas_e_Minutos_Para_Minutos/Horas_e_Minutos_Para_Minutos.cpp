9ff16000190d574adf8b548dfb10cf399291857b
Gabriel Arcanjo <garcanjo938@gmail.com>
2022-10-04 19:48:56 -0300

    Horas e Minutos para Minutos em C++

---
+++ Horas_e_Minutos_Para_Minutos/Horas_e_Minutos_Para_Minutos.cpp
@@ -0,0 +1,15 @@
+/* id do porblema: 142 */
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int H, M;
+
+    cin >> H >> M;
+
+    cout << (H * 60) + M << endl;
+
+    return 0;
+}
