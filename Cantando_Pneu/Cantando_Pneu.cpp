866db92aafb623940155473d0860a5939ceba909
Gabriel Arcanjo <garcanjo938@gmail.com>
2022-10-03 21:15:30 -0300

    Cantando Pneu em C++

---
+++ Cantando_Pneu/Cantando_Pneu.cpp
@@ -0,0 +1,16 @@
+/* id do problema: 1398 */
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int N;
+
+    cin >> N;
+
+    for (int i = 0; i < N; i++){
+        cout << "Pneu" << endl;
+    }
+
+    return 0;
+}
