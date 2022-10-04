401579ba7a7408262bc302f031c00cdd9a3311d4
Gabriel Arcanjo <garcanjo938@gmail.com>
2022-10-03 21:09:58 -0300

    Pfizer em C++

---
+++ Pfizer/Pfizer.cpp
@@ -0,0 +1,11 @@
+/* id do prblema: 1697 */
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    
+    cout << "Bolsonaro, aqui quem fala eh ela a Pfizer. Ta passada???";
+
+    return 0;
+}
