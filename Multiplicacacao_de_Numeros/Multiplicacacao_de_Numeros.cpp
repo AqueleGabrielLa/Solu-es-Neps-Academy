d6e14762ab513f4b0f3176018ed4e1b1e50d503d
Gabriel Arcanjo <garcanjo938@gmail.com>
2022-10-03 21:12:59 -0300

    Multiplicação de Números em C++

---
+++ Multiplicacacao_de_Numeros/Multiplicacacao_de_Numeros.cpp
@@ -0,0 +1,16 @@
+/* id do problema: 1957*/
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    long long int A, B, C;
+	
+	cin >> A >> B;
+	
+	C = A * B;
+	
+	cout << C;
+
+    return 0;
+}
