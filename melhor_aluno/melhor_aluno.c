e3ecf51f4020c4c6e85f909141c3cf7fdc8f0c77
gabriel <garcanjo938@gmail.com>
2022-10-12 17:34:56 -0300

    Melhor Aluno em C

---
+++ melhor_aluno/melhor_aluno.c
@@ -0,0 +1,16 @@
+/* id do problema: 213 */
+#include <stdio.h>
+
+int main(){    	
+    double n1, n2;
+	
+	scanf("%lf%lf", &n1 , &n2);
+	
+	if(n1 <= n2){
+		printf("Pedro");
+	} else {
+		printf("Paulo");
+	}
+
+    return 0;
+}
