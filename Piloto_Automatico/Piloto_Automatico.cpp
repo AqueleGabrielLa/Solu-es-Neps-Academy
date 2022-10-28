773fad3f039557eb73558d7f8c5fc961c0ce0271
gabriel <garcanjo938@gmail.com>
2022-10-28 11:47:56 -0300

    Piloto Automático em C++

---
+++ Piloto_Automatico/Piloto_Automatico.cpp
@@ -0,0 +1,20 @@
+/* id do problema: 890 */
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int A, B, C;
+    
+    cin >> A >> B >> C;
+    
+    if((B - A) < (C - B)){
+        cout << "1";
+    } else if((B - A) > (C - B)){
+        cout << "-1";
+    } else{
+        cout << "0";
+    }
+
+    return 0;
+}
